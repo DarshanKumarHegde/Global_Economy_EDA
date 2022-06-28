923b165ba1ebe3c7ee03e9d36f6ec9632bf0212a
Darshan Hegde <darshan.hegde.dh@gmail.com>
2022-06-28 21:45:32 +0530

    Added API calls for all required Indicators and formed a final Dataframe

--- Data_ETL.py
+++ Data_ETL.py
@@ -3,35 +3,69 @@ import math
 import pandas as pd
 import requests as req
 import json
+import numpy as np
+
+#getting required countries from the  countries API of worldbank as a table
+countriesAPI = 'https://api.worldbank.org/v2/country?format=json&per_page=300'
+response = req.get(countriesAPI)
+print(response.status_code)
+countryDF = pd.DataFrame(response.json()[1])[['id','name', 'capitalCity']]
+countryDF.replace('',np.nan, inplace=True)
+countryDF.dropna().reset_index(drop=True).loc[:,['id','name']]
 
 #getting  variables necessary for API calls
+#countryIDs= ';'.join(countryDF['id']).lower() #country id formatted for the API
+indicators = pd.read_excel('Data_Extract_From_World_Development_Indicators_Metadata.xlsx', 
+                            sheet_name='Series - Metadata')[['Required','Code','Indicator Name']]
+indicators.dropna().drop(columns=['Required'])
+indicatorList = indicators['Code'].tolist()
+print('indicators are', indicatorList)
 
-indicators = []
-countries = [] #take 20 countries with largest gdp
 
 parameters={'country':'all',
-            'id':'SP.POP.TOTL',
-            'start_date':'1980',
-            'end_date':'2020',
-            'page':'1'
+            'id':'IDOfTheIndicator',
+            'startDate':'1990',
+            'endDate':'2020',
+            'pages':1
                 }
-population_data = pd.DataFrame()
-for page in range(100,102):
-    parameters['page']=page
-    responded = req.get("https://api.worldbank.org/v2/country/{country}/indicator/{id}?date={start_date}:{end_date}&format=json&page={page}".format_map(parameters))
-    #responded = req.get("https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?date=2000:2020&format=json&page={}".format(page))
-    #print(type(responded))
-    responded=responded.json()[1]
-    temp_df = pd.DataFrame(responded)[['country', 'countryiso3code', 'date', 'value', 'unit',
-       'obs_status', 'decimal']]
-    temp_df.rename(columns={'value':'population'},inplace=True)
-    print(population_data.columns)
-    temp_df = pd.concat([temp_df, temp_df["country"].apply(pd.Series)], axis=1)
-    population_data = pd.concat([temp_df, population_data], ignore_index=True)
-    print(page, " pages done")
 
-population_data.drop(columns=['country'],inplace=True)
-population_data.rename(columns={'id':'country_code','value':'country_name'},inplace=True)
-print(population_data.head(5))
-print(population_data.describe())
+data = pd.DataFrame()
+finalData = pd.DataFrame()
+#for each Indicator, get data from the API
+for indicator in indicatorList:
+    parameters['id']=indicator
+    getTotalPages = req.get("https://api.worldbank.org/v2/country/{country}/indicator/{id}?date={startDate}:{endDate}&format=json".format_map(parameters))
+    print(getTotalPages.status_code)
+    totalPages=getTotalPages.json()[0]['pages']
 
+    for page in range(1,totalPages):
+        parameters['pages'] = page
+        getDataForIndicator = req.get("https://api.worldbank.org/v2/country/{country}/indicator/{id}?date={startDate}:{endDate}&format=json&page={pages}".format_map(parameters))
+        print(getDataForIndicator.status_code)
+        getDataForIndicator=getDataForIndicator.json()[1]
+        temp_data = pd.DataFrame(getDataForIndicator)[['country', 'countryiso3code', 'date', 'value', 'unit',
+        'obs_status', 'decimal']]
+        data=pd.concat([data, temp_data])
+        print(page,' pages done')
+
+        #indName = indicators.loc[indicator,'Indicator Name']
+    data.rename(columns={'value':indicator},inplace=True)
+    data = pd.concat([data, data["country"].apply(pd.Series)], axis=1)
+    data.drop(columns=['country'], inplace=True)
+    data.rename(columns={'id':'country code','value':'country'}, inplace=True)
+    
+    if finalData.empty:
+        finalData = data
+        finalData.drop(columns=['unit','obs_status','decimal'],inplace=True)
+        print('Dataframe was empty')
+        data=pd.DataFrame()
+    else:
+        finalData = pd.merge(finalData,
+                        data[['countryiso3code','country code','country', 'date',indicator]],
+                        how='left', 
+                        on=['countryiso3code','country code','country','date',])
+        data=pd.DataFrame()
+print(finalData.head(50))
+print(finalData.columns, finalData.shape, finalData.describe())
+
+finalData.to_csv('IndicatorsData.csv')
